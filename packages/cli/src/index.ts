ae58b9c0b1c7f4cd9cfd582ad3a28904f0157912
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2025-03-19 17:59:59 +0900

    init astro docs and turborepo

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,7 +9,7 @@ program
   .description(
     "Easily add instructions, cheat sheets and rules for your favorite tools tailored for your framework."
   )
-  .version("0.0.10");
+  .version("0.0.11");
 
 // Register commands
 initCommand(program);
